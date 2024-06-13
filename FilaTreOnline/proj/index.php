a0db874bd69f03c27df6749c77529fc5d10a96c1
vittorioPiotti <108131700+vittorioPiotti@users.noreply.github.com>
2024-06-13 14:49:23 +0200

    Update index.php

--- FilaTreOnline/proj/index.php
+++ FilaTreOnline/proj/index.php
@@ -1,3 +1,11 @@
+<!--
+  FilaTre Online v1.0.0 (https://github.com/vittorioPiotti/FilaTreOnline)
+  Copyright 2024 Vittorio Piotti
+  Licensed under GPL-3.0 (https://github.com/vittorioPiotti/FilaTreOnline/blob/main/LICENSE.md)
+-->
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
